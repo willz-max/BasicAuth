00f4e18e18a38cd1cc1c3cb45c26a7b2fae22e46
caintheterrible <caintheterrible@gmail.com>
2025-02-02 13:13:37 +0000

    ADDED: initialization point

--- backend/config/deployment/__init__.py
+++ backend/config/deployment/__init__.py
@@ -2,22 +2,22 @@
 import sys
 import os
 from dotenv import load_dotenv
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from config.settings.development import ROOT_URLCONF, ALLOWED_HOSTS
 
 load_dotenv()
 _env= os.environ.get('DJANGO_ENV').lower()
 print('Deploying environment...')
 
 if _env== 'development':
-    import settings.development as dev
-    ALLOWED_HOSTS=[]
-    if not ALLOWED_HOSTS:
-        ALLOWED_HOSTS= ['localhost', '127.0.0.1']
-        print('Development environment deployed successfully!')
+    from config.settings.development import *
+    ALLOWED_HOSTS=ALLOWED_HOSTS
+    ROOT_URLCONF= ROOT_URLCONF
+    print('Development environment deployed successfully!')
+
 elif _env== 'production':
-    import settings.production as prod
+    import config.settings.production as prod
     print('Production environment deployed successfully!')
+
 else:
     raise ValueError(
         f'Unexpected DJANGO_ENV variable!'
